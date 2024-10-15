a0398d4ea709d6a7da9d2202364bad51bf47ab75
Om Prakash <omprakashraj100078@gmail.com>
2024-10-15 13:57:44 +0530

    Today Enhance Project

--- jarvis_app/models.py
+++ jarvis_app/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class SearchQuery(models.Model):
-    query = models.CharField(max_length=255, unique=True)  # User query
-    result = models.TextField()  # Search result (could be a cached snippet or summary)
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp of when the query was made
+    query = models.CharField(max_length=255, unique=True)
+    result = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.query
